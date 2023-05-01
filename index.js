a02c159e4fe95fff3e79607625215183d613d14b
israel diaz <israel.diaz1701@gmail.com>
2023-05-01 07:30:36 -0700

    final js functions cont lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(a = "roller-skate"){
+    return `This Saturday, I want to ${a}!`;
+}
+
+const mondayWork = function (c = "go to the office"){
+    return `This Monday, I will ${c}.`;
+}
+
+function wrapAdjective (d = "*")  {
+    const innerFunction = function (g = "special"){
+        return `You are ${d}${g}${d}!`;
+    };
+    return innerFunction;
+}
+
+
+
